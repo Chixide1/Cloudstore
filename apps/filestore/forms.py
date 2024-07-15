084442689eebcccedfaa7e29f867d8978d1520d9
Chik <chixide12@gmail.com>
2024-07-15 15:43:14 +0100

    added form

--- apps/filestore/forms.py
+++ apps/filestore/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField(widget=forms.FileInput(attrs={'class': 'topbar__upload-hidden',
+    file = forms.FileField(widget=forms.FileInput(attrs={'hidden': '',
                                                          'id': 'upload-hidden', 
-                                                         'onchange':'this.form.submit()'}),
+                                                         }),
                                                          label='')
--- apps/filestore/views.py
+++ apps/filestore/views.py
@@ -1,3 +1,4 @@
+from time import sleep
 from django.http import HttpResponse, HttpRequest, HttpResponseNotAllowed
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
@@ -15,12 +16,21 @@ def dashboard(request):
 def upload_file(request: HttpRequest):
     if request.method == 'POST' and request.FILES['file']:
         data =  request.FILES['file']
-
         File.objects.create(file_data=data,
                             file_name=data.name,
                             file_size=data.size,
                             user=request.user)
-        return HttpResponse()
+        return HttpResponse("""
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <i class="fa-solid fa-circle-check"></i><span class="ps-2">File uploaded successfully.</span>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        """)
     else:
-        return HttpResponse()
+        return HttpResponse("""
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <i class="fa-solid fa-triangle-exclamation"></i><span class="ps-2">Could not upload file.</span>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        """)
     
